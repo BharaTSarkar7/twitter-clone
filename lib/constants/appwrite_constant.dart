1aaaf3a8be733879d591de7aea6e43f2db53991d
BharaT <98583588+BharaTSarkar7@users.noreply.github.com>
2023-02-23 11:34:47 +0530

    Update appwrite_constant.dart

--- lib/constants/appwrite_constant.dart
+++ lib/constants/appwrite_constant.dart
@@ -3,7 +3,7 @@ class AppwriteConstants {
 
   static const String projectId = '63eeff50af59c2b16ec6';
 
-  static const String endPoint = 'http://172.16.71.65:80/v1';
+  static const String endPoint = 'http://Write the ip address of current wifi device:80/v1';
 
   static const String userCollections = '63f6f4bd02fddc9ffb2f';
 }
